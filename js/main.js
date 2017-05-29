c073c9893873171647c064a0c601a160d22778a3
paul <paul.christophe6@gmail.com>
2017-05-29 16:38:27 +0200

    build

--- js/main.js
+++ js/main.js
@@ -1,24 +1,3 @@
-function rest(method, data) {
-  method = method || "GET";
-  data = data || {};
-  var id = data['id'],
-      url = "/store/" + ((id) ? id : '');
+const test = () => 'hi'
 
-  $.ajax({
-    url: url,
-    contentType: "application/json",
-    type: method,
-    data: JSON.stringify(data),
-    dataType: "json"
-  }).done(function(data) {
-    console.log('success', data);
-  }).fail(function(xhr, textStatus, error) {
-    console.log("ERROR", xhr.responseText);
-  });
-}
-
-$(document).ready(function() {
-
-  console.log("document ready");
-
-});
+console.log(`webpack build ${test()}`);
