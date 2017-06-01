16bd278cbfb46c4d15c444a851a69dceea21e64c
Erik Nygren <sten.erik.nygren@gmail.com>
2017-05-31 23:02:10 -0400

    jakes 25 min sesh

--- js/main.js
+++ js/main.js
@@ -4,21 +4,56 @@ function rest(method, data) {
   var id = data['id'],
       url = "/store/" + ((id) ? id : '');
 
-  $.ajax({
+  return $.ajax({
     url: url,
     contentType: "application/json",
     type: method,
     data: JSON.stringify(data),
     dataType: "json"
-  }).done(function(data) {
-    console.log('success', data);
-  }).fail(function(xhr, textStatus, error) {
-    console.log("ERROR", xhr.responseText);
-  });
+  })
 }
 
 $(document).ready(function() {
 
-  console.log("document ready");
+  var serverNameInput = $('.js-name');
+  var serverList = $('.js-server-list');
+  var servers = [];
+
+  function _renderServers() {
+     serverList.html(`<ul class="server-list">${ servers.map(_renderServer).join('') }</ul>`);
+  }
+
+  function _renderServer(server) {
+    return `<li class="server"><input class="server-name js-server-name" value="${server.name}" data-id="${server.id}"></li>`;
+  }
+
+  rest().done(function (data) {
+    servers = data;
+    _renderServers();
+  });
+
+  $.get({
+    url: '/server-name'
+  }).done(function(data) {
+    $('.js-example').text(data.name);
+  });
+
+  _renderServers();
+
+  $('.js-add').on('click', function () {
+    rest('POST', { name: serverNameInput.val() }).done(function (server) {
+      servers.push(server);
+      _renderServers();
+    });
+  });
+
+  serverList.on('blur', '.js-server-name', function (event) {
+    var element = $(this);
+
+    rest('PUT', {
+      id: element.data('id'),
+      name: element.val()
+    });
+  })
 
 });
